9afd43f13d496b0016a3941ca7f5967ff5557354
puffyCid <16283453+puffyCid@users.noreply.github.com>
2025-03-30 22:24:50 +0000

    Initial rustdocs  (#67)

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Data that is currently extracted includes:
 
 An example binary is available to download
 
-- `unifiedlog_iterator` - Can parse a logarchive into a JSOL or CSV file. It can also run
+- `unifiedlog_iterator` - Can parse a logarchive into a JSOL or CSV file. It can also parse the logs
   on a live system. The output file will be quite large
 
 ## Limitations
@@ -43,7 +43,7 @@ the Unified Log format there are some limitations:
 1. No printf style error code lookup support. This library does not do any error
    code lookups for log messages. The native `log` command on macOS supports
    error code lookups when it encounters printf style `%m` messages.\
-   An example base log messsage: 'Failed to open file, error: %m'\
+   For example the log message: 'Failed to open file, error: %m'\
    a. This Library outputs:
    ```
    Failed to open file, error: 1
@@ -53,12 +53,11 @@ the Unified Log format there are some limitations:
    Failed to open file, error: no such file or directory
    ```
 
-2. Support for most custom objects in log messages. However, some objects are
-   not supported. Unsupported objects will be base64 encoded
+2. This library supports most custom objects in log messages. However, unsupported objects will be base64 encoded
 
 # References
 
-https://github.com/ydkhatri/UnifiedLogReader\
-https://github.com/libyal/dtformats/blob/main/documentation/Apple%20Unified%20Logging%20and%20Activity%20Tracing%20formats.asciidoc\
-https://eclecticlight.co/2018/03/19/macos-unified-log-1-why-what-and-how/\
-https://www.crowdstrike.com/blog/how-to-leverage-apple-unified-log-for-incident-response/
+- https://github.com/ydkhatri/UnifiedLogReader
+- https://github.com/libyal/dtformats/blob/main/documentation/Apple%20Unified%20Logging%20and%20Activity%20Tracing%20formats.asciidoc
+- https://eclecticlight.co/2018/03/19/macos-unified-log-1-why-what-and-how
+- https://www.crowdstrike.com/blog/how-to-leverage-apple-unified-log-for-incident-response
--- src/lib.rs
+++ src/lib.rs
@@ -32,20 +32,80 @@
     clippy::unnecessary_cast
 )]
 
+//! # A library to parse Apple Unified Logs
+//! `macos_unifiedlogs` is a small cross platform library to help parse Unified Logs on a system or logarchive.
+//! No Apple APIs are used so this library can be used on non-apple platforms.
+//!
+//! A full example can found on [GitHub](https://github.com/mandiant/macos-UnifiedLogs)
+//! ## Example
+//! ```rust
+//!    use macos_unifiedlogs::filesystem::LiveSystemProvider;
+//!    use macos_unifiedlogs::traits::FileProvider;
+//!    use macos_unifiedlogs::parser::collect_timesync;
+//!    use macos_unifiedlogs::iterator::UnifiedLogIterator;
+//!    use macos_unifiedlogs::unified_log::UnifiedLogData;
+//!    use macos_unifiedlogs::parser::build_log;
+//!
+//!    // Run on live macOS system
+//!     let mut provider = LiveSystemProvider::default();
+//!     let timesync_data = collect_timesync(&provider).unwrap();
+//!
+//!     // We need to persist the Oversize log entries (they contain large strings that don't fit in normal log entries)
+//!     let mut oversize_strings = UnifiedLogData {
+//!        header: Vec::new(),
+//!        catalog_data: Vec::new(),
+//!        oversize: Vec::new(),
+//!     };
+//!     for mut entry in provider.tracev3_files() {
+//!         println!("TraceV3 file: {}", entry.source_path());
+//!         let mut buf = Vec::new();
+//!         entry.reader().read_to_end(&mut buf);
+//!         let log_iterator = UnifiedLogIterator {
+//!             data: buf,
+//!             header: Vec::new(),
+//!         };
+//!         // If we exclude entries that are missing strings, we may find them in later log files
+//!         let exclude = true;
+//!         for mut chunk in log_iterator {
+//!             chunk.oversize.append(&mut oversize_strings.oversize);
+//!             let (results, _missing_logs) = build_log(
+//!                 &chunk,
+//!                 &mut provider,
+//!                 &timesync_data,
+//!                 exclude,
+//!             );
+//!             oversize_strings.oversize = chunk.oversize;
+//!             println!("Got {} log entries", results.len());
+//!             break;
+//!         }
+//!         break;
+//!     }
+//!
+//! ```
+
+/// Functions to parse catalog information from tracev3 files
 mod catalog;
 mod chunks;
 mod chunkset;
+/// Parsers to extract specific log objects
 mod decoders;
+/// Functions to parse the shared string cache
 pub mod dsc;
 mod error;
+/// Providers to parse Unified Log data on a live system or a provided logarchive
 pub mod filesystem;
 mod header;
 pub mod iterator;
+/// Functions to assemble the log message
 mod message;
+/// Functions to extract and assemble log entries from the macOS Unified Log
 pub mod parser;
 mod preamble;
+/// Functions to parse time data associated with the Unified Log
 pub mod timesync;
 pub mod traits;
+/// Functions to parse tracev3 files
 pub mod unified_log;
 mod util;
+/// Functions to parse the log string files
 pub mod uuidtext;
